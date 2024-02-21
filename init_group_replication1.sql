84ba5954d0a4b98bfe42bb88e78ce56b3c910ec6
Akoji Anthony <akojianthony@gmail.com>
2024-02-21 15:09:14 +0100

    Create init_group_replication1.sql

---
+++ init_group_replication1.sql
@@ -0,0 +1,10 @@
+SET SQL_LOG_BIN=0;
+CREATE USER 'repl'@'%' IDENTIFIED WITH mysql_native_password BY 'password';
+GRANT REPLICATION SLAVE ON *.* TO 'repl'@'%';
+FLUSH PRIVILEGES;
+SET SQL_LOG_BIN=1;
+CHANGE MASTER TO MASTER_USER='repl', MASTER_PASSWORD='password' FOR CHANNEL 'group_replication_recovery';
+INSTALL PLUGIN group_replication SONAME 'group_replication.so';
+SET GLOBAL group_replication_bootstrap_group=ON;
+START GROUP_REPLICATION;
+SET GLOBAL group_replication_bootstrap_group=OFF;
